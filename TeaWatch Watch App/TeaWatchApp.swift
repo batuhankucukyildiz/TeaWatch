51bf250ba7990af450429c16ac4a88272e5970fe
Batuhan <kucukyildizbatuhan@gmail.com>
2024-01-08 22:26:58 +0300

    Initial Commit

---
+++ TeaWatch Watch App/TeaWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeaWatchApp.swift
+//  TeaWatch Watch App
+//
+//  Created by Batuhan Küçükyıldız on 8.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TeaWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
